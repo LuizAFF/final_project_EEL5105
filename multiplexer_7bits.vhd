9f6e2a55289ccd03d889342fcc54d1e09469ba36
Leon Held <leonardo.held@grad.ufsc.br>
2018-11-10 18:42:59 -0200

    Multiplexer com Process é retardo mental.

--- multiplexer_7bits.vhd
+++ multiplexer_7bits.vhd
@@ -7,17 +7,13 @@ entity multiplexer_7bits is
 			Sel : in std_logic_vector(1 downto 0);
 			Output : out std_logic_vector(6 downto 0)
 			);
-end entity multiplexer_7bits;
+end entity;
  
 architecture bhv of multiplexer_7bits is
-begin 
-   process (a, b, c, d, Sel) is
-   begin
-      case Sel is
-         when "00"  => Output <= a;
-         when "01"  => Output <= b;
-         when "10"  => Output <= c;
-         when "11"  => Output <= d;
-      end case;
-   end process;
-end architecture bhv;
+begin
+      with Sel select
+         Output <= a when "00",
+         	   b when "01",
+        	   c when "10",
+          	   d when others; 
+end architecture;
