23e44543ff6e8f758b00b392ad77a00c872ecb15
Leon Held <leonardo.held@grad.ufsc.br>
2018-11-10 18:45:43 -0200

    Mesma coisa que o  final_project_EEL5105/multiplexer_7bits.vhd

--- multiplexer.vhd
+++ multiplexer.vhd
@@ -7,17 +7,13 @@ entity multiplexer is
 			Sel : in std_logic_vector(1 downto 0);
 			Output : out std_logic
 			);
-end entity multiplexer;
+end entity;
  
 architecture bhv of multiplexer is
 begin 
-   process (a, b, c, d, Sel) is
-   begin
-      case Sel is
-         when "00"  => Output <= a;
-         when "01"  => Output <= b;
-         when "10"  => Output <= c;
-         when "11"  => Output <= d;
-      end case;
-   end process;
-end architecture bhv;
+      with Sel select
+         Output <= a when "00",
+         	   b when "01",
+        	   c when "10",
+		  d when others; 
+end architecture;
